e1e14148f458b69ce85b6e70a015ace4dbe6a730
Vishvam Mazumdar <30667536+v7770x@users.noreply.github.com>
2020-03-17 13:04:45 -0400

    added constants file for constants to be used in the project

---
+++ constants.py
@@ -0,0 +1,14 @@
+#all of the constants to be used in the project
+
+pi = 3.14159265358979323846
+h = 6.62607004e-34
+h_bar = h/(2*pi)
+c = 299792458
+k = 1.380649e-23
+m_p = 1.67262192369e-27
+M_sun = 1.989e30
+R_sun = 696340000
+L_sun = 3.828e26
+G = 6.674e-11
+
+
